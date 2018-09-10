fced9fbc767531836bdf8d248148d4bfbc580847
syavanaaldrey <42725325+syavanaaldrey@users.noreply.github.com>
2018-09-10 04:25:49 -0700

    Update rekursif.php

--- rekursif.php
+++ rekursif.php
@@ -1,10 +1,40 @@
 <?php
+	function rek($column){
+		global $a;
+		rek2($a, $column);
+		echo "<br>";
+		if (--$column > $a) {
+			rek($column);
+		}
+	}
+	function rek2($b, $column){
+		global $a;
+		if ($b > $a - $column) {
+			echo "*";
+			rek2($b - 1, $column);
+		}
+	}
+	$a = 0;
+	rek(5);
 
-function rekursif($i){
-  echo "$i <br>";
-  $i--;
-  if($i>0)
-    rekursif($i);
+	echo "<br>/*==========================================================================================*/<br><br>";
+
+	function rekursif($c, $d) {
+	global $kolom;
+    rekursif2($kolom, $c, $d);
+    echo "<br>";
+    if (++$c < $kolom){
+        rekursif($c, $d);
+    }
 }
-rekursif(5);
+function rekursif2($e, $c, $f) {
+	global $kolom;
+    if ($e > $kolom - $c - 1) {
+        echo "$f ";
+        rekursif2($e - 1, $c, $f + 1);
+    }
+}
+
+$kolom = 5;
+rekursif(0,1);
 ?>
